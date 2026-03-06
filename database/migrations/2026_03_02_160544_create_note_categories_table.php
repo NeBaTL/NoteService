b33c128d53946a339a1e809cdb0eace6d89ed6e7
Simen_code <semn.kuznecov.06@gmail.com>
2026-03-06 21:11:41 +0500

    update migrations

--- database/migrations/2026_03_02_160544_create_note_categories_table.php
+++ database/migrations/2026_03_02_160544_create_note_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('note_categories', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name');
             $table->timestamps();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             
